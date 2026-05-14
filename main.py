43521803c2b512770c08fcb7371fc69e992f8f5e
vasylrdoc-hash <vasylrdoc@gmail.com>
2026-05-15 01:18:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
 
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+BOT_TOKEN = "ТУТ_ТВІЙ_TOKEN"
 
 async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
     await update.message.reply_text(f"Отримано: {text}")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+app = Application.builder().token(BOT_TOKEN).build()
 
 app.add_handler(MessageHandler(filters.TEXT, handle))
 
